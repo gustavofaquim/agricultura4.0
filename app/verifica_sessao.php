a0efa21756c9a112ca3edd9431432664098bbe6e
Gustavo Faquim <gustavofaquim408@gmail.com>
2022-02-21 22:14:08 -0300

    Finalizando sistema login

--- app/verifica_sessao.php
+++ app/verifica_sessao.php
@@ -1,9 +1,9 @@
 
 <?php
 
-if(($_SESSION['logado']) == "SIM"){
+if(isset($_SESSION['logado'])){
     //echo("Logado ");
-    var_dump($_SESSION);
+    //var_dump($_SESSION);
     //header("Location: ../app/views/pages/home.php");
     //echo "<script> window.location.href = '?i=dashboard'; </script>";
     //exit();
