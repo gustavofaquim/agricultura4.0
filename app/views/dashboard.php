3639a502da7197155b7d067074afc6b71cb07318
Gustavo Faquim <gustavofaquim408@gmail.com>
2022-03-21 19:25:21 -0300

    Correção de gráficos.

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -107,9 +107,15 @@ if(isset($_GET['retornos'])){
     ?>
 
 
-
+<?php 
+            $sensorCT = new SensorController();
+            $sensoresG = $sensorC->listar_por_tipo(3,$_SESSION['central_cod']);
+            foreach($sensoresG as $id => $sensorG){
+                echo "[".substr($sensorG->__get('dt_hr'),9,2).", ".$sensorG->__get('valor')."],";           
+        }    
+        ?>
                     
-<!-- TEMPERATURA -->
+<!-- UMIDADE -->
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script type="text/javascript">
       google.charts.load('current', {'packages':['line']});
@@ -124,25 +130,14 @@ if(isset($_GET['retornos'])){
     
     
       data.addRows([
-        [1,  37.8],
-        [2,  30.9],
-        [3,  100.0],
-        [4,  11.7],
-        [5,  11.9],
-        [6,   8.8],
-        [7,   7.6],
-        [8,  12.3],
-        [9,  16.9],
-        [10, 12.8],
-        [11,  5.3],
-        [12,  6.6],
-        [13,  4.8],
-        [14,  4.2]
+        <?php 
+            $sensorCT = new SensorController();
+            $sensoresG = $sensorCT->listar_por_tipo(1,$_SESSION['central_cod']);
+            foreach($sensoresG as $id => $sensorG){
+                echo "[".substr($sensorG->__get('dt_hr'),8,2).", ".$sensorG->__get('valor')."],";           
+        }    
+        ?>
       ]);
-
-
-      
-
       
 
       var options = {
@@ -174,9 +169,9 @@ if(isset($_GET['retornos'])){
       data.addRows([
         <?php 
             $sensorCT = new SensorController();
-            $sensoresG = $sensorCT->listar_por_tipo(3);
+            $sensoresG = $sensorCT->listar_por_tipo(3,$_SESSION['central_cod']);
             foreach($sensoresG as $id => $sensorG){
-                echo "[".substr($sensorG->__get('dt_hr'),8,2).", ".$sensorG->__get('valor')."],";           
+                echo "[".substr($sensorG->__get('dt_hr'),9,2).", ".$sensorG->__get('valor')."],";           
         }    
         ?>
       ]);
